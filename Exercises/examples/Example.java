e67734331302845fee6325085bf5743c47f7a1d1
ravdaian <Ruben.Avdaian@harman.com>
2019-05-16 14:16:09 +0300

    added class examples from 1 & 2 java sessions

---
+++ Exercises/examples/Example.java
@@ -0,0 +1,38 @@
+package project1;
+
+
+
+class Ball
+{
+	int x;
+	int y;
+	public Ball(int xPos, int yPos) // constructor
+	{
+		this.x = xPos;
+		this.y = yPos;
+	}
+	public Ball()
+	{
+		
+	}
+	public void printInfo()
+	{
+		System.out.println("I am a ball at " + x + ":" + y);
+		System.out.println("My address = " + this);
+	}
+}
+
+public class Example {
+
+	public static void main(String[] argsadfadf) {
+         short x = 5;
+         byte b = (byte) x;
+         System.out.println(b);
+
+	}
+
+	
+	
+
+
+}
